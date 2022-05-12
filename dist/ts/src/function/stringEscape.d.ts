6a119049ba69adbdb2652fa1c8390372795aa9ab
codehyouka <plants.coordinators@gmail.com>
2022-05-12 12:54:16 +0800

    update comment and ts

--- dist/ts/src/function/stringEscape.d.ts
+++ dist/ts/src/function/stringEscape.d.ts
@@ -5,11 +5,11 @@ export default stringEscape;
  * @since 1.3.1
  * @category Seq
  * @param {number} value The second number in an addition.
- * @param {string} type The second number in an addition.
+ * @param {string=} type The second number in an addition.
  * @returns {string} Returns the total.
  * @example
  *
  * stringEscape("yahii & adad ^ss")
  *=> 'yahii&nbsp;&amp;&nbsp;adad&nbsp;&circ;ss'
  */
-declare function stringEscape(value: number, type: string): string;
+declare function stringEscape(value: number, type?: string | undefined): string;
--- dist/ts/src/function/stringUnEscape.d.ts
+++ dist/ts/src/function/stringUnEscape.d.ts
@@ -5,11 +5,11 @@ export default stringUnEscape;
  * @since 1.3.1
  * @category Seq
  * @param {number} value The second number in an addition.
- * @param {string} type The second number in an addition.
+ * @param {string=} type The second number in an addition.
  * @returns {string} Returns the total.
  * @example
  *
  * stringUnEscape('yahii&nbsp;&amp;&nbsp;adad&nbsp;&circ;ss')
  *=>"yahii & adad ^ss"
  */
-declare function stringUnEscape(value: number, type: string): string;
+declare function stringUnEscape(value: number, type?: string | undefined): string;
