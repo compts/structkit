c5c0568ee7f8278e79901b174458016ae15d35eb
codehyouka <plants.coordinators@gmail.com>
2024-12-28 23:06:02 +0800

    Fixing ts release

--- dist/ts/src/function/isExact.d.ts
+++ dist/ts/src/function/isExact.d.ts
@@ -5,7 +5,7 @@ export default isExact;
  * @since 1.0.1
  * @category Collection
  * @param {any} whereValue Json or Array
- * @param {any} objectValue1 Json or Array for lookup to objectValue1
+ * @param {any} objectValue1 Json or Array for lookup to whereValue
  * @param {boolean=} isExist Default value is True
  * @returns {boolean|any} Returns the boolean if the has the value you are looking at.
  * @example
